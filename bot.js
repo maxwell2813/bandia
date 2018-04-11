b255355b9c6913e2f7f8a6ea7cb28f4e3d20e5a6
maxwell2813 <maxwell281333@gmail.com>
2018-04-11 17:00:25 +1000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+
+function commandIs(str, msg){
+    return msg.content.toLowerCase().startsWith("!" + str)
+}
+
+client.on('ready', () => {
+    console.log('The bot is online!');
+});
+
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/);
+	
+    if(commandIs("bandia", message)){
+        message.channel.sendMessage('Yes, Sir!');
+    }
+
+    if(commandIs("Bandia", message)){
+        message.channel.sendMessage('Yes, Sir!');
+    }
+    
+    if(commandIs("BanDia", message)){
+        message.channel.sendMessage('Yes, Sir!');
+    }    
+})
+
+client.login(process.env.BOT_TOKEN);
